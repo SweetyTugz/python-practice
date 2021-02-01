513fee5b82bac72b80701d3f3c4a69c18a5125e2
Sweety <sweettugz@gmail.com>
2021-02-02 02:00:36 +0530

    random practice

--- practice.py
+++ practice.py
@@ -1,1 +1,16 @@
-print("asd")
+# print("asd")
+
+# a = [2,3,5,6]
+map = {1:"one", 2:"two", 3:"three"}
+
+#  item -> map.items()
+# val- > map.values()
+# map.keys()
+# if 1 in map.keys():
+#     print("there")
+# print(list(map.values())[0])
+
+# if "one" in (map.values()):
+#     print("thereee")
+# for key,val in enumerate(map):
+#     print(val)
