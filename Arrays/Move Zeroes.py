ca520b0a6a16c2552175e345c41141ed6d139311
Sweety <sweettugz@gmail.com>
2021-02-05 20:49:31 +0530

    Move Zeroes

---
+++ Arrays/Move Zeroes.py
@@ -0,0 +1,25 @@
+# a=[0,1,0,3,12]
+# c=[]
+# count=0
+# for i in range(len(a)):
+#     if(a[i]!=0):
+#         c.append(a[i])
+#     if(a[i]==0):
+#         count=count+1
+# print(count)
+# x=0
+# while x < count:
+#     c.append(0)
+#     x=x+1
+# print(c)
+
+a=[0,1,0,3,12]
+# for i in range(len(a)):
+#     if(a[i]==0):
+#         a[i:]
+# print(a)
+for i in range(len(a)):
+    if (a[i] == 0):
+        a.remove(a[i])
+        a.append(0)
+print(a)
