ab57b3d6080cfcbd3c21a03d9c4dc4032ef7d3b2
Sweety <sweettugz@gmail.com>
2021-02-08 16:53:29 +0530

    ..

---
+++ Arrays/AddBinary.py
@@ -0,0 +1,12 @@
+# a=10
+# b=3
+# num1=bin(a)
+# num2=num1[2::]
+# num3=bin(b)
+# num4=num3[2::]
+# print(num2+num4)
+
+a='11'
+b='1'
+sum=int(a,2)+int(b,2)
+print(bin(sum)[2::])
--- Arrays/Mutate_the_array.py
+++ Arrays/Mutate_the_array.py
@@ -1,10 +1,10 @@
-a=[4,0,1,-2,3]
+a=[9]
 n=len(a)
 b=[]
 for i in range(n):
     print(a[i])
     if (i == 0):
-        m = 0 + a[0 + a[1]]
+        m = 0 + a[0] + a[1]
         b.append(m)
     else:
         if(i==n-1):
@@ -20,3 +20,24 @@ for i in range(n):
     # # z = a[+1]
     # # b.append(x+y+z)
 print(b)
+
+n=123456
+# l=str(n)
+# count=0
+# m=[int(x) for x in l]
+# for i in range(len(m)):
+#
+#     if(m[i:]%3==0):
+#         count=count+1
+# print(count)
+#
+# # pairs=[[3,5],[1,4],[2,4],[7,8],[8,0]]
+# # c=[]
+# # d=[]
+# # for i in range(len(pairs)):
+# #     x=pairs[i]
+# #     for j in range(len(x)):
+# #         c.append(x[j])
+# #     for l in range(4):
+# #         d.append(c[i])
+# #     print(d)
